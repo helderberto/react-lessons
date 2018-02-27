50e55754cbf66089ee6d801bc44ed0f8b09c5a67
Helder Burato Berto <helder.burato@gmail.com>
2018-02-27 17:39:54 -0300

    Add m01 - part II - Using events

--- lessons/m01/learning-props/src/app.js
+++ lessons/m01/learning-props/src/app.js
@@ -1,17 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
 import Square from './square'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <Title name='Helder' lastname={{ first: 'Burato', last: 'Berto' }} />
-        {['blue', 'orange', 'green'].map((square, index) => (
-          <Square key={index} color={square} />
-        ))}
+      <div className='container' onClick={(e) => {
+        alert('clicou')
+      }}>
+        <Square />
       </div>
     )
   }
