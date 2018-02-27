475b197cd928ca47b23451583d3345aa94207a81
Helder Burato Berto <helder.burato@gmail.com>
2018-02-27 17:21:29 -0300

    Add m01 - part II - Knowing the key property

--- lessons/m01/learning-props/src/app.js
+++ lessons/m01/learning-props/src/app.js
@@ -2,12 +2,16 @@
 
 import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
       <div>
         <Title name='Helder' lastname={{ first: 'Burato', last: 'Berto' }} />
+        {['blue', 'orange', 'green'].map((square) => (
+          <Square key={square} color={square} />
+        ))}
       </div>
     )
   }
