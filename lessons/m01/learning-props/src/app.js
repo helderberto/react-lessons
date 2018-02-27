13ee380daaa1d295eff93d236274cc6366fa330d
Helder Burato Berto <helder.burato@gmail.com>
2018-02-27 16:24:29 -0300

    Add m01 - part II - Learning props

---
+++ lessons/m01/learning-props/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './title'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Title name='Helder' />
+      </div>
+    )
+  }
+}
+
+export default App
