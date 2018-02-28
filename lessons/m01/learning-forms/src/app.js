1172d689d0105e08b2a45219dbe84ef63fea4b7a
Helder Burato Berto <helder.burato@gmail.com>
2018-02-28 11:54:20 -0300

    Add m01 - part II - Learning forms

---
+++ lessons/m01/learning-forms/src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='container'>
+        <form>
+          <input type='text' defaultValue='valor inicial' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
