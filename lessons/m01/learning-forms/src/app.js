4f8de543fbe74d098510d1f18fca5234f5dc0ee3
Helder Burato Berto <helder.burato@gmail.com>
2018-02-28 12:08:46 -0300

    Add m01 - part II - Learning forms: checkbox / radio

--- lessons/m01/learning-forms/src/app.js
+++ lessons/m01/learning-forms/src/app.js
@@ -4,7 +4,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial'
+      value: 'Valor inicial',
+      checked: false
     }
   }
 
@@ -15,6 +16,20 @@ class App extends Component {
           <input type='text' value={this.state.value} onChange={(e) => {
             this.setState({ value: e.target.value })
           }} />
+
+          <label>
+            <input
+              type='checkbox'
+              value='my-checkbox'
+              checked={this.state.checked}
+              onChange={(e) => {
+                this.setState({ checked: e.target.checked })
+              }} />
+            Checkbox
+          </label>
+
+          <input type='radio' name='rd' value='1' /> Radio 1
+          <input type='radio' name='rd' value='2' defaultChecked /> Radio 2
         </form>
       </div>
     )
