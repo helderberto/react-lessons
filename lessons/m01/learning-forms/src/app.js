4711b6ddc9cd13bcec1711436d8110131b490d19
Helder Burato Berto <helder.burato@gmail.com>
2018-02-28 11:57:46 -0300

    Add m01 - part II - Learning forms: Controlled / uncontrolled

--- lessons/m01/learning-forms/src/app.js
+++ lessons/m01/learning-forms/src/app.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'Valor inicial'
+    }
+  }
+
   render () {
     return (
       <div className='container'>
         <form>
-          <input type='text' defaultValue='valor inicial' />
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({ value: e.target.value })
+          }} />
         </form>
       </div>
     )
